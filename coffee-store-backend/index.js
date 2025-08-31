e7354d76028a9cf13e7bfd73f6b8f1b472e471ad
simplysamiul <simplysamiul@gmail.com>
2025-08-31 18:40:37 +0600

    made coffee added functionality and successfully added two coffee in the database

--- coffee-store-backend/index.js
+++ coffee-store-backend/index.js
@@ -12,8 +12,6 @@ app.use(express.json());
 
 
 // mongodb connection
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@coffee-store-cluster.1cs0hzq.mongodb.net/?retryWrites=true&w=majority&appName=coffee-store-cluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,12 +27,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // databse and database collection name
+        const coffeeStoreCollection = client.db("coffee_store").collection("coffee_store_colledtion")
+
+        // coffee add route
+        app.post("/add-coffee", async(req,res) => {
+            const newCoffee= req.body;
+            const result = await coffeeStoreCollection.insertOne(newCoffee);
+            res.send(result);
+        });
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
--- coffee-store-frontend/src/components/dashboard/AddCoffee.jsx
+++ coffee-store-frontend/src/components/dashboard/AddCoffee.jsx
@@ -1,9 +1,31 @@
-
+ import { toast } from 'react-toastify';
 
 const AddCoffee = () => {
     const handelAddCoffeeForm = (e) => {
         e.preventDefault();
         const form = e.target;
+        const name = form.name.value;
+        const supplier = form.supplier.value;
+        const category = form.category.value;
+        const photoURL = form.photoURL.value;
+        const chef = form.chef.value;
+        const taste = form.taste.value;
+        const details = form.details.value;
+        const quantity = form.quantity.value;
+        const coffeeInfo = {name, supplier, category, photoURL, chef, taste, details, quantity};
+        // send coffee in the databse
+        fetch("http://localhost:5000/add-coffee",{
+            method: "POST", 
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(coffeeInfo)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.acknowledged && data.insertedId){
+                toast.success("Coffee Added Successfully .....!", {theme: "colored"});
+                form.reset();
+            }
+        }).catch(err => toast.success(err.message, {theme: "colored"}))
     }
     return (
         <div>
@@ -15,11 +37,11 @@ const AddCoffee = () => {
                     <div className="flex flex-col lg:flex-row justify-between lg:gap-12">
                         <div className="w-full">
                             <legend className="fieldset-legend text-chocoDark text-lg mt-6">Name</legend>
-                            <input type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter Coffee Name" />
+                            <input required name="name" type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter Coffee Name" />
                             <legend className="fieldset-legend text-chocoDark text-lg mt-6">Supplier</legend>
-                            <input type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter Supplier Name" />
-                            <legend className="fieldset-legend text-chocoDark text-lg mt-6">Browsers</legend>
-                            <select defaultValue="Pick a browser" className="select w-full border-1 border-chocoLight text-chocoDark">
+                            <input required name="supplier" type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter Supplier Name" />
+                            <legend className="fieldset-legend text-chocoDark text-lg mt-6">Coffee Category</legend>
+                            <select required name="category" defaultValue="Pick a browser" className="select w-full border-1 border-chocoLight text-chocoDark">
                                 <option disabled={true}>Coffee Category</option>
                                 <option>Cappuccino</option>
                                 <option>Latte</option>
@@ -27,15 +49,17 @@ const AddCoffee = () => {
                                 <option>Americano</option>
                             </select>
                             <legend className="fieldset-legend text-chocoDark text-lg mt-6">Coffee Photo</legend>
-                            <input type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter Coffee Photo URL" />
+                            <input required name="photoURL" type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter Coffee Photo URL" />
                         </div>
                         <div className="w-full">
                             <legend className="fieldset-legend text-chocoDark text-lg mt-6">Chef</legend>
-                            <input type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter coffee chef" />
-                            <legend className="fieldset-legend text-chocoDark text-lg mt-6">Taste</legend>
-                            <input type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter coffee taste" />
+                            <input required name="chef" type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter coffee chef" />
+                            <legend className="fieldset-legend text-chocoDark text-lg mt-6">Price</legend>
+                            <input required name="taste" type="number" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter coffee price" />
                             <legend className="fieldset-legend text-chocoDark text-lg mt-6">Details</legend>
-                            <input type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter coffee details" />
+                            <input required name="details" type="text" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Enter coffee details" />
+                            <legend className="fieldset-legend text-chocoDark text-lg mt-6">Quantity</legend>
+                            <input required name="quantity" type="number" className="input w-full border-1 border-chocoLight text-chocoDark" placeholder="Available quantity" />
                         </div>
                     </div>
                     <button className="w-full mt-6 btn border-1 border-chocoDark bg-chocoLight text-chocoDark font-secondary text-xl hover:bg-chocoDark hover:text-chocoLight duration-300">Add Coffee</button>
