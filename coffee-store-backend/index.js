1ff8f06461edd26ea2e94dcfdc0f409153643234
simplysamiul <simplysamiul@gmail.com>
2025-10-25 19:39:26 +0600

    update redme file

--- coffee-store-backend/index.js
+++ coffee-store-backend/index.js
@@ -1,10 +1,9 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 
 // use middleware
 app.use(cors());
@@ -26,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         // databse and database collection name
         const coffeeStoreCollection = client.db("coffee_store").collection("coffee_store_colledtion");
@@ -104,8 +103,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
